81d9755fc1480afdf8086df8850bcd8343933b82
rNexeR <rnexer@gmail.com>
2016-08-20 01:00:38 -0600

    Uploading initial code

---
+++ app.js
@@ -0,0 +1,37 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , cors = require('cors');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 8000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  //app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.use(cors());
+
+
+app.options('*', cors());
+
+app.get('/', cors(), routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port %s in %s mode.",  app.get('port'), app.settings.env);
+});
