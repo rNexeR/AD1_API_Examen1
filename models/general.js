81225d1e040ec6dca7cea6e7d14651146485964d
rNexeR <rnexer@gmail.com>
2016-08-20 23:15:13 -0600

    Fixing errors

---
+++ models/general.js
@@ -0,0 +1,65 @@
+var connection = require('../connection');
+
+function activities() {
+    this.get = function (table, res) {
+        connection.acquire(function(err, con){
+            con.query('select * from ' + table, function (err, result) {
+                con.release();
+                if(err)
+                    res.json(500, {Error: err});
+                else
+                    res.json(200, result);
+            })
+        });
+    };
+
+    this.create = function (table, activity, res) {
+        connection.acquire(function (err, con) {
+            con.query('insert into ' + table +' set ?', activity, function (err, result) {
+                con.release();
+                if(err)
+                    res.json(500, {Message: 'Creacion de ' + table +' fallida'});
+                else    
+                    res.json(200, {Message: 'Creacion de ' + table +' exitosa'});
+            });
+        });
+    };
+
+    this.update = function (table, activity, res) {
+        connection.acquire(function (err, con) {
+            con.query('update ? set ? where id_actividad = ?', [table,activity, activity.id_Actividad], function (err, result) {
+                con.release();
+                if(err)
+                    res.json(500, {Message: 'Actualizacion de ' + table +' fallida'});
+                else    
+                    res.json(200, {Message: 'Actualizacion de ' + table +' exitosa'});
+            });
+        });
+    }
+
+    this.delete = function (table, id, res) {
+        connection.acquire(function (err, con) {
+            con.query('delete from ? where id_actividad = ?', [table,id], function (err, result) {
+                con.release();
+                if(err)
+                    res.json(500, {Message: 'Eliminacion de ' + table +' fallida'});
+                else    
+                    res.json(200, {Message: 'Eliminacion de ' + table +' exitosa'});
+            });
+        });
+    }
+
+    this.getOne = function (table, id, res) {
+        connection.acquire(function(err, con){
+            con.query('select * from ' + table +' where id_actividad = ?', [table,id], function (err, result) {
+                con.release();
+                if(err)
+                    res.json(500, {Error: err});
+                else
+                    res.json(200, result);
+            })
+        });
+    };
+}
+
+module.exports = new activities();
--- routes/activities_routes.js
+++ routes/activities_routes.js
@@ -1,25 +1,25 @@
-var activities = require('../models/actividades');
+var activities = require('../models/general');
 
 module.exports = {
     configure: function (app) {
         app.get('/actividades', function (req, res) {
-            activities.get(res);
+            activities.get('actividades', res);
         });
 
         app.get('/actividades/:id', function (req, res) {
-            activities.getOne(req.params.id, res);
+            activities.getOne('actividades', req.params.id, res);
         });
 
         app.post('/actividades', function (req, res) {
-            activities.create(req.body, res);
+            activities.create('actividades', req.body, res);
         });
 
         app.put('/actividades', function (req, res) {
-            activities.update(req.body, res);
+            activities.update('actividades', req.body, res);
         });
 
         app.delete('/actividades/:id', function (req, res) {
-            activities.delete(req.params.id, res);
+            activities.delete('actividades', req.params.id, res);
         });
 
     }
