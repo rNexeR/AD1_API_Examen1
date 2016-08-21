3daca3bca81c9d9500ed396e144faa19cb6f731d
rNexeR <rnexer@gmail.com>
2016-08-21 12:54:41 -0600

    Connection

--- connection.js
+++ connection.js
@@ -8,8 +8,18 @@ function Connection() {
       connectionLimit: 10,
       host: '',
       user: 'root',
+      //Nexer
+      ///*
+      password: 'Password123!!',
+      database: 'ad1_exa1'
+      //*/
+
+      //Josue
+      /*
       password: 'pass123',
       database: 'ad1_t1'
+      */
+      
     });
   };
 
